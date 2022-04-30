bebc69084c352cdac57a7192099925e5dc50eb7c
Mukesh <mukesh192k@gmail.com>
2022-04-30 12:29:37 +0530

    png to gcode have been succesfully created

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stencilview import StencilView
 from kivy.graphics import Color, Ellipse, Line, Rectangle
+import test
 
 
 class Float_layout(FloatLayout):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.painter = MyPaintWidget(size_hint=(
@@ -15,21 +18,28 @@ class Float_layout(FloatLayout):
         with self.painter.canvas:
             Color(0, 0, 0)
             Rectangle(pos=self.painter.pos, size=self.painter.size)
+        self.infotext = "hey"
         self.ssbtn = Button(text='Save', size_hint=(
             0.1, 0.05), pos_hint={"x": 0, "y": 0.1})
         self.clearbtn = Button(text='Clear', size_hint=(
             0.2, 0.05), pos_hint={"x": 0.1, "y": 0.1})
+        # self.la = Label(text=self.infotext, font_size='20sp',
+        #                 color=[0.41, 0.42, 0.74, 1], ids="fun")
         self.ssbtn.bind(on_release=self.save_screenshot)
         self.clearbtn.bind(on_release=self.clear_screen)
+        # self.add_widget(self.la)
         self.add_widget(self.painter)
         self.add_widget(self.ssbtn)
         self.add_widget(self.clearbtn)
 
     def save_screenshot(self, obj):
         self.painter.export_to_png("screenshot.png")
+        test.run()
 
     def clear_screen(self, obj):
         self.painter.canvas.clear()
+        self.infotext += "hey"
+        self.ids.fun.text = self.infotext
 
 
 class MyPaintWidget(StencilView):
@@ -56,7 +66,7 @@ class MyPaintApp(App):
         root.bind(size=self._update_rect, pos=self._update_rect)
 
         with root.canvas.before:
-            Color(0, 1, 0, 1)  # green; colors range from 0-1 not 0-255
+           # Color(0, 1, 0, 1)  # green; colors range from 0-1 not 0-255
             self.rect = Rectangle(size=root.size, pos=root.pos)
         return root
 
--- test.py
+++ test.py
@@ -1,10 +1,27 @@
 from pylab import imread
-from stl_tools import numpy2stl
-
-from scipy.misc import lena, imresize
+from csdt_stl_tools import numpy2stl
 from scipy.ndimage import gaussian_filter
+import os
 
-A = 256 * imread("screenshot.png")
-A = A[:, :, 2] + 1.0*A[:, :, 0]  # Compose RGBA channels to give depth
-A = gaussian_filter(A, 1)  # smoothing
-numpy2stl(A, "NASA.stl", scale=0.05, mask_val=5., solid=True)
+
+def run():
+    filename = "this.stl"
+    A = 256 * imread("screenshot.png")
+    A = A[:, :, 2] + 1.0*A[:, :, 0]  # Compose RGBA channels to give depth
+    A = gaussian_filter(A, 1)  # smoothing
+    p = numpy2stl(A, "fun.stl", scale=0.05,
+                  mask_val=5., solid=True, max_width=70)
+    p = str(p)
+    p = p.split("\\n")
+    l = 0
+    file1 = open(filename, "w")  # append mode
+    file1.write("")
+    file1.close()
+    file1 = open(filename, "a")
+    for i in p:
+        if l == 0:
+            l += 1
+            continue
+        file1.write(i+"\n")
+    file1.close()
+    os.system(f"slic3r-console -o this.gcode --load config.ini {filename}")
