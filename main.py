99b5ace0211021964206a8b4dea77e60cdc78cad
Mukesh <mukesh192k@gmail.com>
2022-05-03 09:09:31 +0530

    configure GCode parsing and reinitiated .gitignore file

--- main.py
+++ main.py
@@ -1,78 +1,66 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.core.window import Window
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.stencilview import StencilView
-from kivy.graphics import Color, Ellipse, Line, Rectangle
-import test
+import time
+from matplotlib.pylab import imread
+from csdt_stl_tools import numpy2stl
+from scipy.ndimage import gaussian_filter
+import os
+from PIL import Image
+import serial
 
 
-class Float_layout(FloatLayout):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.painter = MyPaintWidget(size_hint=(
-            0.7, 0.5), pos_hint={"x": 0, "y": 0.5})
-        with self.painter.canvas:
-            Color(0, 0, 0)
-            Rectangle(pos=self.painter.pos, size=self.painter.size)
-        self.infotext = "hey"
-        self.ssbtn = Button(text='Save', size_hint=(
-            0.1, 0.05), pos_hint={"x": 0, "y": 0.1})
-        self.clearbtn = Button(text='Clear', size_hint=(
-            0.2, 0.05), pos_hint={"x": 0.1, "y": 0.1})
-        # self.la = Label(text=self.infotext, font_size='20sp',
-        #                 color=[0.41, 0.42, 0.74, 1], ids="fun")
-        self.ssbtn.bind(on_release=self.save_screenshot)
-        self.clearbtn.bind(on_release=self.clear_screen)
-        # self.add_widget(self.la)
-        self.add_widget(self.painter)
-        self.add_widget(self.ssbtn)
-        self.add_widget(self.clearbtn)
-
-    def save_screenshot(self, obj):
-        self.painter.export_to_png("screenshot.png")
-        #  test.run()
-
-    def clear_screen(self, obj):
-        self.painter.canvas.clear()
-        self.infotext += "hey"
-        self.ids.fun.text = self.infotext
+def serialConnector():
+    serialPort = serial.Serial(port="/dev/ttyUSB0", baudrate=115200,
+                               bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    i = 100000
+    while i:
+        print(serialPort.read())
+        i -= 1
+    time.sleep(5)
+    print("Start")
+    res = bytes("G28", 'utf-8')
+    serialPort.write(res)
+    time.sleep(4)
+    serialPort.close()
 
 
-class MyPaintWidget(StencilView):
-   # StencilView.canvas(Color((1, 0.5, 0.5)))
-    with StencilView().canvas.after:
-        Color(0, 0, 1)
-        #Rectangle(pos=self.painter.pos, size=self.painter.size)
-
-    def on_touch_down(self, touch):
-        color = (1, 1, 1)
-        with self.canvas:
-            Color(*color, mode='hsv')
-            d = 5
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y), width=(d))
-
-    def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
+def par():
+    a = ""
+    with open("this.gcode") as f:
+        a += "".join(line for line in f if not line.isspace())
+    a = a.splitlines()
+    i = 0
+    for each in a:
+        if ';' in each:
+            each = each[:each.index(';')]
+            if each == '':
+                continue
+        print(each)
 
 
-class MyPaintApp(App):
-    def build(self):
-        self.root = root = Float_layout()
-        root.bind(size=self._update_rect, pos=self._update_rect)
+def run():
+    filename = "this.stl"
+    A = 256 * imread("screenshot.png")
+    A = A[:, :, 2] + 1.0*A[:, :, 0]  # Compose RGBA channels to give depth
+    A = gaussian_filter(A, 1)  # smoothing
+    p = numpy2stl(A, "fun.stl", scale=0.05,
+                  mask_val=5., solid=True, max_width=70)
+    print("Converion of STL complete")
+    p = str(p)
+    p = p.split("\\n")
+    l = 0
+    file1 = open(filename, "w")  # append mode
+    file1.write("")
+    file1.close()
+    file1 = open(filename, "a")
+    for i in p:
+        if l == 0:
+            l += 1
+            continue
+        file1.write(i+"\n")
+    file1.close()
+    print("Writing of STL complete")
+    os.system(f"slic3r-console -o this.gcode --load config.ini {filename}")
+    print("Gcode produced")
 
-        with root.canvas.before:
-           # Color(0, 1, 0, 1)  # green; colors range from 0-1 not 0-255
-            self.rect = Rectangle(size=root.size, pos=root.pos)
-        return root
 
-    def _update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
-
-
-MyPaintApp().run()
+par()
+# run()
---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt install python3
+apt install pip
+pip install pyserial
+pip install matplotlib
+pip install csdt_stl_tools
+pip install PIL
+
+
+
