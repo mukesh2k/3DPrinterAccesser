2aabb9d93883b5f77184e64b9c5a407f221e0313
Mukesh <mukesh192k@gmail.com>
2022-05-29 18:55:58 +0530

    bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import threading
+#import RPi.GPIO as GPIO
 from matplotlib.pylab import imread
 from csdt_stl_tools import numpy2stl
 from scipy.ndimage import gaussian_filter
@@ -14,29 +15,35 @@ portal = "/dev/ttyUSB0"
 cache = ""
 
 
-def serialReader():
-    global serialPort
-    serialPort = serial.Serial(port=portal, baudrate=115200,
-                               bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-    serialPort.close()
-    serialPort.open()
-    while 1:
-        print(serialPort.readline())
-    return
-
-
-def serialWriter(gcode):
-    p = printcore(portal, 115200)
-    gcode = gcoder.LightGCode(gcode)
-    while not p.online:
-        time.sleep(0.1)
-    p.startprint(gcode)
-    while p.printing():
-        pass
+def fileScanner():
+    print("Waiting for files to print")
+    lis = []
+    while len(lis) == 0:
+        lis = os.listdir(pather)
+        lis = [i for i in lis if "." in i and (
+            i[i.index('.'):] == '.png' or i[i.index('.'):] == '.txt')]
+    sort_list = sorted(lis)
+    name = sort_list[0]
+    if name[name.index('.'):] == ".png":
+        try:
+            w = int(name[name.index('_')+1:name.rindex('_')])
+            h = int(name[name.rindex('_')+1:name.index('.')])
+        except:
+            print("Invalid file name configuration. File deleted")
+            os.system(f"rm {pather}{sort_list[0]}")
+            fileScanner()
+            return
+        PNG2STL(f"{pather}{name}", int(w), int(h), False)
+    else:
+        f = open(f"{pather}{name}", 'r')
+        braille(f.read())
+    os.system(f"rm {pather}{sort_list[0]}")
+    print("done")
+    pass
 
 
 framesize = (300, 400)
-hori_limit = 22
+hori_limit = 13
 veri_limit = 10
 
 
@@ -60,89 +67,56 @@ def braille(inputString):
             curline += 1
         if curline == veri_limit:
             bg = Image.new('RGBA', framesize)
-
             draw = ImageDraw.Draw(bg)
             draw.text((100, 100), convertedstring, font=font)
-            bg.save('out.png')
-            PNG2Gcode("out.png", 2.5, 70, True)
+            bg.save(f'{pather}out.png')
+            PNG2STL(f"{pather}out.png", 2.5, 70, True)
             convertedstring = ""
             curline = 0
             cur = 0
+            print(
+                "Take your print out. Remaining words will start print after 60 seconds")
+            a = 60
+            while a >= 0:
+                a -= 1
+                print(a)
+                time.sleep(1)
     if cur != 0 or curline != 0:
         bg = Image.new('RGBA', framesize, (0, 0, 0))
         draw = ImageDraw.Draw(bg)
         draw.text((10, 10), convertedstring,
                   font=font, fill=(255, 255, 255))
-        bg.save('out.png')
-    PNG2Gcode("out.png", 2.5, 70, True)
+        bg.save(f'{pather}out.png')
+    PNG2STL(f"{pather}out.png", 2.5, 70, True)
     print(convertedstring)
 
 
-def fileScanner():
-    global pather
-    # pather = "/home/mukesh/Documents/test/"
-    pather = "/home/pi/Desktop/3Dprinter/"
-    print("running")
-    lis = []
-    while len(lis) == 0:
-        lis = os.listdir(pather)
-        lis = [i for i in lis if "." in i and (
-            i[i.index('.'):] == '.png' or i[i.index('.'):] == '.txt')]
-    sort_list = sorted(lis)
-    name = sort_list[0]
-    if name[name.index('.'):] == ".png":
-        w = name[name.index('_')+1:name.rindex('_')]
-        h = name[name.rindex('_')+1:name.index('.')]
-        PNG2Gcode(name, w, h, False)
-    else:
-        f = open(f"{pather}{name}", 'r')
-        braille(f.read())
-    os.system(f"rm {pather}{sort_list[0]}")
-    print("done")
-    pass
-
-
-def GcodeParser():
-    a = ""
-    with open(f"{pather}this.gcode") as f:
-        a += "".join(line for line in f if not line.isspace())
-    a = a.splitlines()
-    gCode_commands = []
-    for each in a:
-        if ';' in each:
-            each = each[:each.index(';')]
-            if each == '':
-                continue
-        # print(each)
-        gCode_commands.append(each)
-    return gCode_commands
-
-
-def PNG2Gcode(pngname, heightmm, widthmm, brallille):
+def PNG2STL(pngname, heightmm, widthmm, brallille):
+    print("Converion of STL started")
     filename = "this.stl"
     if brallille == True:
         bg = Image.new('RGBA', framesize, (255, 255, 255))
         font = ImageFont.truetype(f"{pather}braille.ttf", 30)
         draw = ImageDraw.Draw(bg)
         draw.text((0, 0), "a", font=font, fill=(0, 0, 0))
-        bg.save('white.png')
+        bg.save(f'{pather}white.png')
         dots = 256 * imread(pngname)
         # Compose RGBA channels to give depth
-        dots = dots[:, :, 2] + 1.*dots[:, :, 0]
+        dots = 1.*dots[:, :, 0]
         dots /= 2
-        surface = 256 * imread("white.png")
+        surface = 256 * imread(f"{pather}white.png")
         os.system(f"rm {pather}out.png")
         os.system(f"rm {pather}white.png")
         # Compose RGBA channels to give depth
         surface = surface[:, :, 2] + 1.*surface[:, :, 0]
         dots += surface/2.25
         dots = gaussian_filter(dots, 1)  # smoothing
-        print("Converion of STL started")
-        p = numpy2stl(dots, "fun.stl", scale=0.03,
+        p = numpy2stl(dots, "fun.stl", scale=0.0235,
                       mask_val=5., solid=True, max_width=150)
     else:
-        dots = 256 * imread(pngname)
-        dots = dots[:, :, 2] + 1.*dots[:, :, 0]
+        print(pngname)
+        dots = 256 * imread(f"{pngname}")
+        dots = 1.*dots[:, :, 0]
         dots = gaussian_filter(dots, 1)  # smoothing
         p = numpy2stl(dots, "fun.stl", scale=heightmm*0.01,
                       mask_val=5., solid=True, max_width=widthmm)
@@ -167,24 +141,67 @@ def STL2Gcode(p, filename):
         file1.write(i+"\n")
     file1.close()
     print("Writing of STL complete")
+    return
     os.system(
-        f"{pather}PrusaSlicer-version_2.4.2-armhf.AppImage -s {pather}{filename} -o {pather}this.gcode")
+        f"{pather}PrusaSlicer-version_2.4.2-armhf.AppImage -s {pather}{filename} --load {pather}config.ini")
     print("Gcode produced")
-    # GcodeParser()
+    GcodeParser()
+
+
+def GcodeParser():
+    a = ""
+    with open(f"{pather}this.gcode") as f:
+        a += "".join(line for line in f if not line.isspace())
+    a = a.splitlines()
+    gCode_commands = []
+    for each in a:
+        if ';' in each:
+            each = each[:each.index(';')]
+            if each == '':
+                continue
+        # print(each)
+        gCode_commands.append(each)
+    print("Done")
+    return gCode_commands
+
+
+def serialReader():
+    global serialPort
+    serialPort = serial.Serial(port=portal, baudrate=115200,
+                               bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    serialPort.close()
+    serialPort.open()
+    while 1:
+        print(serialPort.readline())
+    return
+
+
+def serialWriter(gcode):
+    p = printcore(portal, 115200)
+    gcode = gcoder.LightGCode(gcode)
+    while not p.online:
+        time.sleep(0.1)
+    p.startprint(gcode)
+    while p.printing():
+        pass
 
 
 text = "Its not such a big problem, but If you paste a transparent background image on a transparent background using Image paste and pass a mass the edge will be black."
 # #text = "you can see there are some black outlines around text. "
-# text = "".join("a\n" for i in range(21))
-# braille(text)
-#PNG2Gcode("download (1).png", 2, 70)
-# Original flow
 # fileScann
 pather = "/home/pi/Desktop/3Dprinter/"
-
+pather = "/home/pi/Desktop/3Dprinter/"
+#pather = "/home/mukesh/Documents/test/"
 if __name__ == "__main__":
-    # while 1:
-    #     t = threading.Thread(target=fileScanner)
-
-    # fileScanner()
-    GcodeParser()
+    # GPIO.setmode(GPIO.BOARD)
+    # GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+    # GPIO.output(18, GPIO.HIGH)
+    while 1:
+        fileScanner()
+        print("Take your print out. Next print will start after 60 seconds")
+        a = 60
+        while a >= 0:
+            a -= 1
+            print(a)
+            time.sleep(1)
+    # GcodeParser()
